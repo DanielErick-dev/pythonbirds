b7c364fea1942adabc30d529d3829455f6eeea52
Daniel Erick <DanielErick.estudante@gmail.com>
2022-07-20 11:37:55 -0300

    criada a classe Pessoa

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,2 +1,16 @@
 class Pessoa:
-    pass
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+
+
+
+    def cumprimentar(self):
+        return f'olá {self.nome} and you are {self.idade} years old'
+
+
+if __name__ == '__main__':
+    p = Pessoa('daniel')
+    print(p.cumprimentar())
+
+
