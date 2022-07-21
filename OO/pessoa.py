9dd9922b462ee80118932b164ec6f5faeb02a4c5
Daniel Erick <DanielErick.estudante@gmail.com>
2022-07-21 09:50:04 -0300

    criando atributos complexos

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,16 +1,17 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
-
+        self.filhos = list(filhos)
 
 
     def cumprimentar(self):
         return f'olá {self.nome} and you are {self.idade} years old'
 
 
+
 if __name__ == '__main__':
-    p = Pessoa('daniel')
-    print(p.cumprimentar())
-
-
+    renzo = Pessoa(nome='renzo')
+    luciano = Pessoa(renzo, nome='luciano')
+    for filho in luciano.filhos:
+        print(filho.nome)
